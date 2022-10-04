f634512d915a6b68f25bb4ce44d4ab4da1d0dd15
HolyGort <115030090+HolyGort@users.noreply.github.com>
2022-10-04 19:46:57 +0300

    Add files via upload

---
+++ Гаусс-Калькулятор/main.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\PROGRA~2\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Program Files (x86)\Python38-32
+"%~dp0.\main.exe" %*
